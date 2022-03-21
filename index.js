d8075074c803f0145f6a5fc074a8d2ab0b172b5e
Shubham <shubham.sharma6396@gmail.com>
2022-03-21 10:02:47 +0530

    Add notes completed

---
+++ index.js
@@ -0,0 +1,46 @@
+let notesObj = [];
+showNotes();
+
+let addBtn = document.getElementById("addBtn");
+
+
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  console.log(notesObj);
+  showNotes();
+});
+
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    let notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `<div class="notes">
+            <h2>Note ${index + 1}</h2>
+            <p>${element}</p>
+            <button class="btn" id="${index}" onclick="deleteNote(this.id)">Delete</button>
+            </div>`;
+  });
+
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  }
+  else{
+    notesElm.innerHTML = `Nothing to show! Please Enter a note using 'Add Note' option above`;
+  }
+}
